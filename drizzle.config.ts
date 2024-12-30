e3e2e458b86043806c585d0be11c62de23e7f26a
Cesar Silva <cesarsilva.dev@gmail.com>
2024-12-30 14:30:59 -0300

    feat: add envs and drizzle setup

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { env } from "@/env.js"
+import type { Config } from "drizzle-kit"
+
+export default {
+  schema: "./db/schema/index.ts",
+  dialect: "postgresql",
+  out: "./drizzle",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+} satisfies Config
